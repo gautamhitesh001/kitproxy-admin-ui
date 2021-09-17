93933f6d8023b66a1d93ad80d7910939e0b53470
adnanAjackus <61192454+adnanAjackus@users.noreply.github.com>
2021-09-17 09:41:56 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { ui_logo, app_version } from "./config/Constants";
-import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./appRedux/store";
+import "./App.scss";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={ui_logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-					<br />
-					<br />
-					Program Version : {app_version}
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<header className="App-header">
+					<img src={ui_logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+						<br />
+						<br />
+						Program Version : {app_version}
+					</p>
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		</Provider>
 	);
 }
 
