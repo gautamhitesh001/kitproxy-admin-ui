4c4001a7169e635e0a1b1832970a8b0b9c7a0d19
adnanAjackus <adnan@ajackus.com>
2021-12-03 15:09:54 +0530

    added card tiles

--- src/components/inputs/configInput/index.js
+++ src/components/inputs/configInput/index.js
@@ -4,43 +4,42 @@ import { Box } from "@mui/system";
 import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
-    btnWrapper: ({ isTabActive }) => ({
-        flex: 1,
-    }),
-    tabBtn: {
-        flex: 1,
-        backgroundColor: "#FFFFFF !important",
-        border: "1px solid #E6E6E6 !important",
-        paddingTop: "20px !important",
-        paddingBottom: "20px !important",
-        color: theme.palette.secondary.main,
-    },
-    activeTabBtn: {
-        flex: 1,
-        backgroundColor: "#FFFFFF !important",
-        border: "1px solid #F4672A !important",
-        paddingTop: "20px !important",
-        paddingBottom: "20px !important",
-        color: theme.palette.primary.main,
-    },
+	btnWrapper: ({ isTabActive }) => ({
+		flex: 1,
+	}),
+	tabBtn: {
+		flex: 1,
+		backgroundColor: "#FFFFFF !important",
+		border: "1px solid #E6E6E6 !important",
+		paddingTop: "20px !important",
+		paddingBottom: "20px !important",
+		color: theme.palette.secondary.main,
+	},
+	activeTabBtn: {
+		flex: 1,
+		backgroundColor: "#FFFFFF !important",
+		border: "1px solid #F4672A !important",
+		paddingTop: "20px !important",
+		paddingBottom: "20px !important",
+		color: theme.palette.primary.main,
+	},
 }));
 
 export const ConfigInput = (props) => {
+	const { inputLabel, inputWidth } = props;
+	const classes = useStyles({ inputWidth });
 
-    const { inputLabel, inputWidth } = props
-    const classes = useStyles({ inputWidth });
-
-    return (
-        <Stack maxWidth={!inputWidth ? "65%" : inputWidth + "px"} direction="column">
-            <Typography marginBottom="4px" variant="subtitle1" color="secondary.60">
-                {inputLabel}
-            </Typography>
-            <TextField {...props} />
-        </Stack>
-    );
+	return (
+		<Stack maxWidth={!inputWidth ? "65%" : inputWidth + "px"} direction="column">
+			<Typography marginBottom="4px" variant="subtitle1" color="secondary.60">
+				{inputLabel}
+			</Typography>
+			<TextField {...props} />
+		</Stack>
+	);
 };
 
 ConfigInput.propTypes = {
-    inputLabel: PropTypes.string,
-    inputWidth: PropTypes.number,
+	inputLabel: PropTypes.string,
+	inputWidth: PropTypes.number,
 };
