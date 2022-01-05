3f022c4fcd9e9f751dfdd99aa0e840321a0822d1
adnanAjackus <adnan@ajackus.com>
2022-01-05 11:54:28 +0530

    fixes in switches

--- src/components/switches/index.js
+++ src/components/switches/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { styled } from "@mui/system";
-import { SwitchUnstyled, switchUnstyledClasses } from "@mui/core";
+import { SwitchUnstyled, switchUnstyledClasses } from "@mui/material";
 
 const Root = styled("span")`
 	font-size: 0;
