9c471921748bb5d9e14ad99b3825fde65d802a31
adnanAjackus <adnan@ajackus.com>
2022-01-12 11:19:28 +0530

    resolved conflicts

--- src/appRedux/actions/Configuration.js
+++ src/appRedux/actions/Configuration.js
@@ -11,3 +11,14 @@ export const getConfigurationSettings = (organizationId, onSuccess) => {
 		// call login service
 	};
 };
+
+export const updateConfigurationSetting = (originalObj, objId, data, onSuccess) => {
+	return (dispatch) => {
+		dispatch({ type: configurationConstants.UPDATE_CONFIGURATION_SETTINGS_REQUEST });
+
+		let settingsData = { ...originalObj };
+		settingsData[objId] = data;
+		dispatch({ type: configurationConstants.UPDATE_CONFIGURATION_SETTINGS_SUCCESS });
+		dispatch({ type: configurationConstants.GET_CONFIGURATION_SETTINGS_SUCCESS, data: settingsData });
+	};
+};
