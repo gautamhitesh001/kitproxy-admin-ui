5440404a7136c7f1224baf8d3c206f77f8a4d6b8
Rahul Pathak <v-rahul.ap@nowfloats.com>
2022-01-20 12:43:52 +0530

    org creation done.

--- src/appRedux/constants/index.js
+++ src/appRedux/constants/index.js
@@ -2,3 +2,4 @@ export * from "./auth.constants";
 export * from "./onboarding.constants";
 export * from "./settings.constants";
 export * from "./configuration.constants";
+export * from "./organization.constant";
--- src/appRedux/store.js
+++ src/appRedux/store.js
@@ -1,12 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    stateReconciler: hardSet,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [];
 middlewares.push(thunkMiddleware);
 if (process.env.REACT_APP_DEBUG_MODE === "true") {
-	const { logger } = require("redux-logger");
-	middlewares.push(logger);
+    const { logger } = require("redux-logger");
+    middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(persistedReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
