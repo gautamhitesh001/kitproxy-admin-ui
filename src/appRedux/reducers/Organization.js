fc0ab653d8fc94b9a4d5f71d9f6566591f4e90ff
Rahul Pathak <v-rahul.ap@nowfloats.com>
2022-02-03 16:38:51 +0530

    org creation issue fixed

--- src/appRedux/reducers/Organization.js
+++ src/appRedux/reducers/Organization.js
@@ -1,28 +1,28 @@
 import { organizationConstants } from "../constants";
 
 const initialValues = {
-    isOrganizationCreated: false,
-    organizationInfo: {},
+	isOrganizationCreated: false,
+	organizationInfo: {},
 };
 
 const Organization = (state = initialValues, action) => {
-    switch (action.type) {
-        case organizationConstants.CREATE_ORGANIZATION_REQUEST:
-        case organizationConstants.GET_ORGANIZATION_REQUEST:
-            return {
-                organizationInfo: {},
-                isOrganizationCreated: false,
-            };
-        case organizationConstants.CREATE_ORGANIZATION_SUCCESS:
-        case organizationConstants.GET_ORGANIZATION_SUCCESS:
-            return {
-                ...state,
-                isOrganizationCreated: true,
-                organizationInfo: action.data,
-            };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case organizationConstants.CREATE_ORGANIZATION_REQUEST:
+		case organizationConstants.GET_ORGANIZATION_REQUEST:
+			return {
+				organizationInfo: {},
+				isOrganizationCreated: false,
+			};
+		case organizationConstants.CREATE_ORGANIZATION_SUCCESS:
+		case organizationConstants.GET_ORGANIZATION_SUCCESS:
+			return {
+				...state,
+				isOrganizationCreated: true,
+				organizationInfo: action.data,
+			};
+		default:
+			return state;
+	}
 };
 
 export default Organization;
